fa278a60b163243dadfac4c12f3730de03f3e802
h0-h0-h0 <46834815+h0-h0-h0@users.noreply.github.com>
2024-02-08 17:58:34 +0000

    Initial node/express project files for delete
    Initial node/express project files for delete

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { resolve } = require('path');
+
+const app = express();
+const port = 3010;
+
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+  res.sendFile(resolve(__dirname, 'pages/index.html'));
+});
+
+app.post('/delete', (req, res) => {
+  console.log(`DELETE received at http://localhost:${port}`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
